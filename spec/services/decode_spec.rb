d9fc7d3f9b53054ecce76f756dc07f7bc0ec85d4
yunchang <thinktheworld.yunchang@gmail.com>
2023-11-22 23:35:01 +0700

    Update Decode spec with adding/correct test case

--- spec/services/decode_spec.rb
+++ spec/services/decode_spec.rb
@@ -6,7 +6,7 @@ describe Decode, type: :service do
   subject(:service) { described_class.new(string) }
   
   describe "#call" do
-    context "when string is empty" do
+    context "when string is nil" do
       let(:string) { nil }
 
       it do
@@ -14,8 +14,16 @@ describe Decode, type: :service do
       end
     end
 
+    context "when string is an empty string" do
+      let(:string) { "" }
+
+      it do
+        expect(service.call).to eq(0)
+      end
+    end
+
     context "when string is not empty" do
-      let(:string) { "005aAc" }
+      let(:string) { "5aAc" }
 
       it do
         expect(service.call).to eq(1232324)
